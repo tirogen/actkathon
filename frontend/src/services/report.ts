14aa3854bb5f9ad574ca1082c67b5fa0f97c24db
N <N@N.N>
2021-08-07 14:02:38 +0700

    report service

--- frontend/src/services/report.ts
+++ frontend/src/services/report.ts
@@ -1,25 +1,21 @@
-import { axiosStore } from './../stores';
-import { Report, ReportResult } from './../types/report';
+import axios from 'axios';
+import { Report } from './../types/report';
 
 class ReportService {
     create(report: Report) {
-        return axiosStore.instance.post('/report', report);
+        return axios.post('/report/create', {
+            topic: report.topic,
+            description: report.detail,
+            latitude: report.lat,
+            longitude: report.lng,
+        });
     }
 
-    async getAll() {
-        const { data } = await axiosStore.instance.get<ReportResult[]>('/report');
-        return data;
-    }
+    async getAll() {}
 
-    async get(reportId: string) {
-        const { data } = await axiosStore.instance.get<ReportResult>(`/report/${reportId}`);
-        return data;
-    }
+    async get(reportId: string) {}
 
-    async vote(reportId: string) {
-        const { data } = await axiosStore.instance.post<ReportResult>(`/report/vote/${reportId}`);
-        return data;
-    }
+    async vote(reportId: string) {}
 }
 
 export default new ReportService();
